34dd347de579c2e519c643f0586c9c4dcb9a4809
Juan Daniel <juanda554242@gmail.com>
2020-12-07 23:23:51 -0500

    integrando bootrap-vue

--- cliente/src/main.js
+++ cliente/src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import {BootstrapVue,IconsPlugin} from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 
