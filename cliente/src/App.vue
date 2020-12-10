aaeb09ddd735b08857c7100bfc3f4bfe5937811d
Juan Daniel <juanda554242@gmail.com>
2020-12-09 19:23:15 -0500

    colocando un componente con vue

--- cliente/src/App.vue
+++ cliente/src/App.vue
@@ -1,59 +1,87 @@
 <template>
   <div id="app">
     <div class="header">
-      
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container">
-          <a class="navbar-brand text-white font-weig" href="#">GesDoc</a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+      <div>
+        <b-navbar toggleable="sm" type="dark" variant="dark">
+          <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+          <b-navbar-brand class="font-weight-bold mr-5"
+            >Cajero Api</b-navbar-brand
           >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Pricing</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+          <b-collapse id="nav-text-collapse" is-nav>
+            <b-navbar-nav class="text-center">
+              <b-nav-item v-on:click="init" v-if="is_auth">Inicio</b-nav-item>
+              <b-nav-item v-on:click="getBalance" v-if="is_auth">Saldo</b-nav-item>
+              <b-nav-item v-if="is_auth">Transaccion</b-nav-item>
+              <b-nav-item v-if="is_auth">Cerrar Sesion</b-nav-item>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-navbar>
+      </div>
     </div>
-    <div class="main-component">
-      <div class="collapse" id="navbarToggleExternalContent">
-        <div class="bg-dark p-4">
-          <h5 class="text-white h4">Collapsed content</h5>
-          <span class="text-muted">Toggleable via the navbar brand.</span>
-        </div>
+
+    <div class="main-component text-center">
+      <jumbotrom></jumbotrom>
+
+    </div>
+
+    <!-- usuario -->
+
+    <div class="m-auto">
+        <b-form class="w-50 m-auto">
+
+          <label class="sr-only" for="inline-form-input-name">Nombre</label>
+          <b-input-group prepend="👤" class="mb-2 mr-sm-2 mb-sm-0 sm-12">
+            <b-form-input
+            type="text"
+              id="inline-form-input-username"
+              placeholder="Juan Daniel Suarez"
+            ></b-form-input>
+          </b-input-group><br>
+          <label class="sr-only" for="inline-form-input-username"
+            >Correo</label
+          >
+          <b-input-group prepend="✉" class="mb-2 mr-sm-2 mb-sm-0">
+            <b-form-input
+            type="email"
+              id="inline-form-input-username"
+              placeholder="jdvpl@gmail.com"
+            ></b-form-input>
+          </b-input-group><br>
+
+          <b-input-group prepend="🔑" class="mb-2 mr-sm-2 mb-sm-0">
+            <b-form-input
+            type="password"
+              id="inline-form-input-username"
+              placeholder="Contraseña"
+            ></b-form-input>
+          </b-input-group><br>
+          <b-button variant="dark" class="btn btn-block">Guardar</b-button>
+        </b-form>
+    </div>
+    <div class="footer">
+      <div class="d-flex bg-dark text-light align-items-center px-3 py-3">
+        <strong class="m-auto">Desarrollado por: Juan Daniel Suarez</strong>
       </div>
     </div>
   </div>
 </template>
 
+😁
 <script>
+
+import Jumbotrom from './components/Jumbotrom.vue';
 export default {
   name: "App",
-  components: {},
+  components: {
+    Jumbotrom
+  },
   data: function () {
     return {
       is_auth: localStorage.getItem("isAuth") || false,
     };
   },
   methods: {},
-  beforeCreate: function () {
+    beforeCreate: function () {
     localStorage.setItem("current_username", "JDVPL");
     localStorage.setItem("isAuth", true);
   },
@@ -61,9 +89,17 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0 0 0 0;
+}
 
-nav{
+nav {
   background-color: rgba(0, 0, 0, 0.521) !important;
-
+}
+.footer {
+  bottom: 0;
+  position: fixed;
+  width: 100%;
+  text-align: center !important;
 }
 </style>
--- cliente/src/main.js
+++ cliente/src/main.js
@@ -2,9 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import {BootstrapVue,IconsPlugin} from 'bootstrap-vue'
+import {BootstrapVue} from 'bootstrap-vue'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 /* eslint-disable no-new */
 new Vue({
